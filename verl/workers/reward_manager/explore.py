0542ce92b2d27df46ad4ff8a5d8d98d98ae80f05
avitrost <avitrost@gmail.com>
2025-06-02 17:17:01 -0700

    fix reward manager

--- verl/workers/reward_manager/explore.py
+++ verl/workers/reward_manager/explore.py
@@ -57,19 +57,6 @@ class ExploreRewardManager:
         import concurrent.futures
 
         def compute_score_for_item(i, data_item):
-            prompt_ids = data_item.batch['prompts']
-            prompt_length = prompt_ids.shape[-1]
-            valid_prompt_length = data_item.batch['attention_mask'][:prompt_length].sum()
-            valid_prompt_ids = prompt_ids[-valid_prompt_length:]
-
-            response_ids = data_item.batch['responses']
-            valid_response_length = data_item.batch['attention_mask'][prompt_length:].sum()
-            valid_response_ids = response_ids[:valid_response_length]
-
-            # decode
-            prompt_str = self.tokenizer.decode(valid_prompt_ids, skip_special_tokens=True)
-            response_str = self.tokenizer.decode(valid_response_ids, skip_special_tokens=True)
-
             history = data_item.non_tensor_batch['history']
 
             # print(f"naive reward manager {self.tokenizer=}")
